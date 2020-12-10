81b154c80b976105e7528d37b360f64024f084e4
beardboi <mr.k3rn3lp4n1c@gmail.com>
2020-12-10 03:57:09 -0300

    feat(MainActivity): implement FastAdapter and RecyclerView

--- app/src/main/java/cl/ucn/disc/dbravo/news/MainActivity.java
+++ app/src/main/java/cl/ucn/disc/dbravo/news/MainActivity.java
@@ -1,14 +1,84 @@
+/*
+ * Copyright 2020 Diego Bravo B
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ *  and associated documentation files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+ *  the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ *  substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ *  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ *  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package cl.ucn.disc.dbravo.news;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.adapters.ModelAdapter;
+
+import java.util.List;
+
+import cl.ucn.disc.dbravo.news.domain.News;
+import cl.ucn.disc.dbravo.news.domain.NewsItem;
+import cl.ucn.disc.dbravo.news.services.System;
+import cl.ucn.disc.dbravo.news.services.SystemImplNewsApi;
+
+/**
+ * The main activity class.
+ *
+ * @author Diego Bravo B.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * OnCreate.
+     *
+     * @param savedInstanceState Used to reload the app.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // The FastAdapter
+        ModelAdapter<News, NewsItem> newsAdapter = new ModelAdapter<>(NewsItem::new);
+        FastAdapter<NewsItem> fastAdapter = FastAdapter.with(newsAdapter);
+        fastAdapter.withSelectable(false);
+
+        // The Recycler View
+        RecyclerView recyclerView = findViewById(R.id.am_rv_news);
+        recyclerView.setAdapter(fastAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+                DividerItemDecoration.VERTICAL));
+
+        // Get the news in the background thread
+        AsyncTask.execute(() -> {
+
+            // Using the contracts
+            System system = new SystemImplNewsApi("dcc476490d624a60a76f2ab30ec11e47");
+
+            // Get the news
+            List<News> newsList = system.retrieveNews(30);
+
+            // Set the adapter
+            runOnUiThread(() -> {
+                newsAdapter.add(newsList);
+            });
+        });
     }
 }
