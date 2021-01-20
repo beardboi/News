b489f8407713f203b06b8d5054ed856543f14a85
Raul Andres Ramos Palominos <Raul.a.ramos.p@gmail.com>
2021-01-20 08:44:07 -0300

    docs(AppCache): Class documentation

--- app/src/main/java/cl/ucn/disc/dbravo/news/Cache/AppCache.java
+++ app/src/main/java/cl/ucn/disc/dbravo/news/Cache/AppCache.java
@@ -8,19 +8,28 @@ import androidx.room.RoomDatabase;
 
 import cl.ucn.disc.dbravo.news.domain.News;
 
+/**
+ * Class AppCache
+ * @author Raul Ramos, Daniel Suares, Diego Bravo
+ */
 @Database(entities = {News.class}, version = 1)
 public abstract class AppCache extends RoomDatabase {
 
+    //CacheDao cache
     public abstract CacheDao cache();
 
+    //Initialization INSTANCE
     public static AppCache INSTANCE;
 
+    /**
+     * Static method to get the instance
+     * @param context
+     * @return Instance
+     */
     public static AppCache getINSTANCE(Context context){
         if(INSTANCE == null){
             INSTANCE = Room.databaseBuilder(context, AppCache.class, "checklist.db")
                     .allowMainThreadQueries().fallbackToDestructiveMigration().build();
-
-
         }
         return INSTANCE;
     }
