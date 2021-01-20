9d150215ebe3d09cb08fda31b83d6c3a95a7d30c
Raul Andres Ramos Palominos <Raul.a.ramos.p@gmail.com>
2021-01-20 08:01:35 -0300

    docs(CacheDao): Class documentation

--- app/src/main/java/cl/ucn/disc/dbravo/news/Cache/CacheDao.java
+++ app/src/main/java/cl/ucn/disc/dbravo/news/Cache/CacheDao.java
@@ -8,14 +8,21 @@ import java.util.List;
 
 import cl.ucn.disc.dbravo.news.domain.News;
 
+/**
+ * Class CacheDao
+ * @author Raul Ramos, Diego Bravo, Daniel Suares
+ */
+@Dao
 public interface CacheDao {
-
+    //Select all news
     @Query("SELECT * FROM News")
     List<News> getAll();
 
+    //Select News by id
     @Query("SELECT * FROM News WHERE id =:id")
     News findById(long id);
 
+    //Insert News in database
     @Insert
     void insert(News news);
 }
