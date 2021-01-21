bf356b2f72230039339ea406ea136180e857a585
beardboi <mr.k3rn3lp4n1c@gmail.com>
2021-01-21 04:17:03 -0300

    feat(ExceptionHandler): add error response method

--- web/app/Exceptions/Handler.php
+++ web/app/Exceptions/Handler.php
@@ -1,12 +1,62 @@
 <?php
+/*
+ * Copyright 2020 Diego Bravo, diego.bravo@alumnos.ucn.cl
+ *                Daniel Suares, daniel.suares@alumnos.ucn.cl
+ *                Raul Ramos, raul.ramos@alumnos.ucn.cl
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
+/**
+ * Class Handler
+ * @package App\Exceptions
+ * @author Diego Bravo B
+ */
 class Handler extends ExceptionHandler
 {
+    /**
+     * This function allows to replace the 404 response with a JSON response.
+     *
+     * @param Request $request The HTTP request.
+     * @param Throwable $e The exception.
+     * @return JsonResponse|Response|\Symfony\Component\HttpFoundation\Response
+     * @throws Throwable
+     */
+    public function render($request, Throwable $e) {
+        // If the exception is an 'Not Found 404 error'
+        if($e instanceof NotFoundHttpException) {
+            // Return a
+            return response()->json([
+                'message' => 'data not found',
+            ], 404);
+        }
+
+        // Return the response
+        return parent::render($request, $e);
+    }
+
     /**
      * A list of the exception types that are not reported.
      *
